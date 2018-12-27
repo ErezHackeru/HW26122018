dca72412f6bad4ef86ea9344adca6e6d97e5316f
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2018-12-27 17:30:11 +0200

    HW_Q_2

---
+++ HW27122018_2/HW27122018_2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW27122018_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    lbl5.Invoke((MethodInvoker)delegate { lbl5.Text = (4-i).ToString(); });
+                    Thread.Sleep(1000);
+                }
+
+
+                lbl5.Invoke((MethodInvoker)delegate { lbl5.Text = "Surprise !"; });
+            }).Start();
+
+            ;
+        }
+    }
+}
